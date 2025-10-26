0f9704440b1f3faa3ddf6969c95c804ea4d7a9d6
Chirag Gulati <chiraggulati098@gmail.com>
2025-10-27 02:34:50 +0530

    init auth user mongo setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost/nest'),
+    AuthModule, 
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

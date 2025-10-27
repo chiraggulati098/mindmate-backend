c6c6334192d8cd8c639b8042ce8dbcf831573a22
Chirag Gulati <chiraggulati098@gmail.com>
2025-10-27 22:35:49 +0530

    init subjects documents

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { SubjectsModule } from './subjects/subjects.module';
+import { DocumentsModule } from './documents/documents.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost/nest'),
     AuthModule, 
-    UsersModule
+    UsersModule, SubjectsModule, DocumentsModule
   ],
   controllers: [AppController],
   providers: [AppService],
